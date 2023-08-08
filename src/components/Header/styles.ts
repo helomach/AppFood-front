1498c06d077ccc8774a82ea74d6f768ba0d0de31
Heloisa Machado <106851899+helomach@users.noreply.github.com>
2023-08-08 20:23:28 -0300

    Add files via upload

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,13 @@
+import { styled } from "styled-components";
+
+export const Container = styled.header`
+    background: #d73035;
+`;
+
+export const Content = styled.div`
+    width: 100%;
+    max-width: 1216px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+`
